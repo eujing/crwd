6dc349bd69e8f3eab27db352daf47d74f8679ae0
Francis <emailforfrancis@gmail.com>
2016-01-23 18:50:50 +0800

    Automatic database population

--- dataServer/DataPopulator/run.bat
+++ dataServer/DataPopulator/run.bat
@@ -1,3 +1,3 @@
-javac -cp "./../sqlite4java.jar;%CLASSPATH%" DataPopulator.java
-java -cp "./../sqlite4java.jar;%CLASSPATH%" DataPopulator
+javac -cp "./../sqlite4java.jar;./commons-csv-1.2.jar;%CLASSPATH%" DataPopulator.java
+java -cp "./../sqlite4java.jar;./commons-csv-1.2.jar;%CLASSPATH%" DataPopulator
 pause
