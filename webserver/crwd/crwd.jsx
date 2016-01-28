73442490aeba997c8baf3510e54b16eb9d5b7fa3
Chua Eu Jing <chuaeujing@gmail.com>
2016-01-28 15:15:18 +0800

    Linux launch scripts, sync requirements and removal of unsafe meteor packages

--- webserver/crwd/crwd.jsx
+++ webserver/crwd/crwd.jsx
@@ -2,7 +2,25 @@ Locations = new Mongo.Collection("locations");
 Sensors = new Mongo.Collection("sensors");
 
 if (Meteor.isClient) {
+
+    Meteor.subscribe("locations");
+    Meteor.subscribe("sensors");
+
     Meteor.startup(function () {
         ReactDOM.render(<App />, document.getElementById("render-target"));
     });
 }
+
+if (Meteor.isServer) {
+    Meteor.publish("locations", function() {
+        return Locations.find();
+    });
+
+    Meteor.publish("sensors", function() {
+        return Sensors.find();
+    });
+}
+
+Meteor.methods({
+    //Server side queries here
+});
